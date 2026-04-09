2a016ca1437777e2c92b33c10a4332fcbc97c0c9
SugarRM <101039083+SugarRM@users.noreply.github.com>
2026-04-09 12:39:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base, Session
 
 TOKEN = "8328136805:AAFtDSd5r9fn5nbKkdcpvdvVn-zlAIDIUNk"
-DB_FILE = "sqlite:////app/data/bot.db"  # Привяжи к persistent disk на
+DB_FILE = "sqlite:////app/data/bot.db"  # Привяжи к persistent disk 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
